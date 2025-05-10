f26ef388b789edb4083c408d67991390547f7a41
DARVIN <4372283+darvinnueza@users.noreply.github.com>
2025-05-10 00:25:02 -0500

    commit
    commit

--- bahai-calendar/vite.config.ts
+++ bahai-calendar/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nombre-del-repositorio/',
+  base: '/cumbahai/',
   plugins: [react()],
 })
